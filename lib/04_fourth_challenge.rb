8a8610160f427bf5cef3f595941845dff7bf55f4
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-11 15:49:10 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,12 +6,12 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {:name => "Benvolio",
-          :age => "17"
-          :attitude =>
+          :age => "17",
+          :attitude => "worried"
         },
-         {:name =>
-          :age =>
-          :attribute =>
+         {:name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
         #your hashes here!
       ]
@@ -21,6 +21,14 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+         {:name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+         {:name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
         #your hashes here!
       ]
    }
